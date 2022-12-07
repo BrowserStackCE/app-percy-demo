1b9bd902751c884e629de066d8ba6ad9fed75f2e
Abhi Singh <abhi.s@browserstack.com>
2022-12-07 19:39:31 +0530

    adds app-upload

---
+++ app/upload.js
@@ -0,0 +1,58 @@
+var axios = require("axios");
+var { createReadStream } = require("fs");
+var FormData = require("form-data");
+
+async function noAppExists(customId) {
+  var config = {
+    url: `https://api-cloud.browserstack.com/app-automate/recent_apps/${customId}`,
+    method: "get",
+    auth: {
+      username: process.env.BROWSERSTACK_USERNAME || "BROWSERSTACK_USERNAME",
+      password:
+        process.env.BROWSERSTACK_ACCESS_KEY || "BROWSERSTACK_ACCESS_KEY",
+    },
+  };
+
+  const axios_response = await axios(config);
+  console.log(`Response - ${JSON.stringify(axios_response.data)}`);
+  return JSON.stringify(axios_response.data).includes("No results found");
+}
+
+async function uploadApp(appPath, customId) {
+  const formData = new FormData();
+  const appPathFS = createReadStream(appPath);
+
+  formData.append("file", appPathFS);
+  formData.append("custom_id", customId);
+
+  var config = {
+    url: "https://api-cloud.browserstack.com/app-automate/upload",
+    method: "post",
+    headers: formData.getHeaders(),
+    auth: {
+      username: process.env.BROWSERSTACK_USERNAME || "BROWSERSTACK_USERNAME",
+      password:
+        process.env.BROWSERSTACK_ACCESS_KEY || "BROWSERSTACK_ACCESS_KEY",
+    },
+
+    data: formData,
+  };
+
+  const axios_response = await axios(config);
+  console.log(
+    `App Uploaded Successfully with response - ${axios_response.data.app_url}`
+  );
+}
+
+async function main() {
+  //upload Android App
+  if (await noAppExists("BStackAppAndroid")) {
+    await uploadApp("./app/browserstack-demoapp.apk", "BStackAppAndroid");
+  }
+  // //Upload iOS App
+  if (await noAppExists("BStackAppIOS")) {
+    await uploadApp("./app/browserstack-demoapp.ipa", "BStackAppIOS");
+  }
+}
+
+main();
--- create-demo-pr.sh
+++ create-demo-pr.sh
@@ -7,33 +7,10 @@ if [ "$BROWSERSTACK_USERNAME" == "" ] || [ "$BROWSERSTACK_ACCESS_KEY" == "" ]; t
   exit 1
 fi
 
-# Check if Android app is uploaded if not then upload new app
-var="$(curl -u "$BROWSERSTACK_USERNAME:$BROWSERSTACK_ACCESS_KEY" -X GET "https://api-cloud.browserstack.com/app-automate/recent_apps/BStackAppAndroid" 2>/dev/null)"
-echo "$var"
-if [[ "$var" =~ ^{.*}$ ]] || [ "$var" == "" ]; then
-  echo "Uploading the Android App"
-  curl -u "$BROWSERSTACK_USERNAME:$BROWSERSTACK_ACCESS_KEY" \
-  -X POST "https://api-cloud.browserstack.com/app-automate/upload" \
-  -F "file=@./app/browserstack-demoapp.apk" \
-  -F "custom_id=BStackAppAndroid"
-fi
-
-# Check if iOS app is uploaded if not then upload new app
-var="$(curl -u "$BROWSERSTACK_USERNAME:$BROWSERSTACK_ACCESS_KEY" -X GET "https://api-cloud.browserstack.com/app-automate/recent_apps/BStackAppIOS" 2>/dev/null)"
-echo "$var"
-if [[ "$var" =~ ^{.*}$ ]] || [ "$var" == "" ]; then
-  echo "Uploading the iOS App"
-  curl -u "$BROWSERSTACK_USERNAME:$BROWSERSTACK_ACCESS_KEY" \
-  -X POST "https://api-cloud.browserstack.com/app-automate/upload" \
-  -F "file=@./app/browserstack-demoapp.ipa" \
-  -F "custom_id=BStackAppIOS"
-fi
-
 if ! [ -x "$(command -v hub)" ]; then
   echo 'Error: hub is not installed (https://hub.github.com/). Please run "brew install hub".' >&2
   exit 1
 fi
-
 
 NOW=`date +%d%H%M%S`
 BASE_BRANCH="main-$NOW"
@@ -59,6 +36,9 @@ git checkout -b $BRANCH
 git commit --amend -m 'Change test details.'
 # git push origin $BRANCH
 # PR_NUM=$(hub pull-request -b $BASE_BRANCH -m 'Change test details.' | grep -oE '[0-9]+')
+
+# Upload Apps to BrowserStack
+npm run upload-app
 
 export PERCY_BRANCH=$BRANCH
 export PERCY_PULL_REQUEST=$PR_NUM
